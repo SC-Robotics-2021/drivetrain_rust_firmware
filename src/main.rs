95991e0c421ae28921fb41a1767485272da7eed4
joshua salzedo <thHunkn0WNd@gmail.com>
2020-05-13 18:31:43 -0700

    Apply RTFM

--- src/main.rs
+++ src/main.rs
@@ -2,54 +2,40 @@
 #![no_main]
 #![no_std]
 
-use cortex_m;
-use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+use heapless;
+use heapless::consts::U64;
 // Halt on panic
 use panic_semihosting as _;
-use stm32f4xx_hal::{delay as hal_delay, prelude::*, pwm, stm32, };
+use rtfm::app;
+use stm32f4::stm32f446::Interrupt;
+use stm32f4xx_hal::{gpio, pwm, prelude::*};
+type PWM0_CHANNEL = gpio::gpioa::PA8<gpio::Alternate<gpio::AF1>>;
 
-#[entry]
-fn main() -> ! {
-    if let (Some(dp), Some(cp)) = (
-        stm32::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the system clock.
-        let rcc = dp.RCC.constrain();
+#[app(device = stm32f4::stm32f446, peripherals = true)]
+const APP: () = {
+    #[init]
+    fn init(context: init::Context) {
+        hprintln!("hello world!").unwrap();
+        let mut rcc = context.device.RCC.constrain();
         let clocks = rcc.cfgr.freeze();
 
-        let mut delay = hal_delay::Delay::new(cp.SYST, clocks);
-
-        let gpioa = dp.GPIOA.split();
+        let gpioa = context.device.GPIOA.split();
         let channels = (
             gpioa.pa8.into_alternate_af1(),
             gpioa.pa9.into_alternate_af1(),
         );
         // configure TIM1 for PWM
-        let pwm = pwm::tim1(dp.TIM1, channels, clocks, 501.hz());
+        let pwm = pwm::tim1(context.device.TIM1, channels, clocks, 501.hz());
         // each TIM has two channels
-        let (mut ch1, mut ch2) = pwm;
+        let (mut ch1, _ch2) = pwm;
         let max_duty = ch1.get_max_duty();
         let min_duty = max_duty / 2;
 
-        let center = max_duty as f32 * 0.75;
-        ch1.set_duty(center as u16);
-        ch2.set_duty(to_scale(max_duty, min_duty, 0.0));
+        ch1.set_duty(to_scale(max_duty, min_duty, 0.0));
         ch1.enable();
-        ch2.enable();
-        loop {
-            ch2.set_duty(to_scale(max_duty, min_duty, 0.25));
-            delay.delay_ms(1000_u32);
-            ch2.set_duty(to_scale(max_duty, min_duty, -0.25));
-            delay.delay_ms(1000_u32);
-            ch2.set_duty(to_scale(max_duty, min_duty, 0.0));
-            delay.delay_ms(1000_u32);
-
-        }
     }
-
-    loop {}
-}
+};
 
 /// Convert `value` from [-1,1] to [new_min, new_max]
 fn to_scale(new_max: u16, new_min: u16, value: f32) -> u16 {
