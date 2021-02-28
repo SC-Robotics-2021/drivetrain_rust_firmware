645aa7cd549a63ac0e668b1cf2c8fec8ad3ba2a6
Joshua Salzedo <thhunkn0wnd@gmail.com>
2021-02-27 16:29:54 -0800

    rename `Jrk` to avoid sheer confusion

--- src/jrk.rs
+++ src/jrk.rs
@@ -1,5 +1,6 @@
 use jrk_g2_rs::JrkG2;
 use stm32f4xx_hal::{gpio, gpio::AlternateOD};
+
 // can't use I2C 1 because all the pins are consumed by other peripherals
 // so we use I2C 2 as it has available pins
 pub type I2c2Sda = gpio::gpiob::PB10<AlternateOD<gpio::AF4>>;
@@ -10,7 +11,7 @@ pub type JrkI2c2 = jrk_g2_rs::JrkG2I2c<I2c2>;
 use crate::utilities::to_scale;
 
 #[allow(dead_code)] // hopefully this stuff is used, but its not the end of the world if its not.
-pub enum Jrk {
+pub enum JrkMapping {
     Default,
     Pitch,
     Rotation,
@@ -19,15 +20,15 @@ pub enum Jrk {
     LowerAxis,
 }
 
-impl Into<u8> for Jrk {
+impl Into<u8> for JrkMapping {
     fn into(self) -> u8 {
         match self {
-            Jrk::Default => 11,
-            Jrk::Pitch => 12,
-            Jrk::Rotation => 13,
-            Jrk::Grip => 14,
-            Jrk::UpperAxis => 15,
-            Jrk::LowerAxis => 16,
+            JrkMapping::Default => 11,
+            JrkMapping::Pitch => 12,
+            JrkMapping::Rotation => 13,
+            JrkMapping::Grip => 14,
+            JrkMapping::UpperAxis => 15,
+            JrkMapping::LowerAxis => 16,
         }
     }
 }
@@ -38,22 +39,26 @@ pub fn set_jrk_pose(
     jrk: &mut JrkI2c2,
     pose: rover_postcards::KinematicArmPose,
 ) -> Result<(), stm32f4xx_hal::i2c::Error> {
-    jrk.set_device(11);
     if let Some(rotation_axis) = pose.rotation_axis {
-        jrk.set_target(to_scale(u16::MAX, u16::MIN, rotation_axis))?;
+        set_jrk(jrk, JrkMapping::Rotation, rotation_axis)?;
     }
     if let Some(upper_axis) = pose.upper_axis {
-        jrk.set_device(12);
-        jrk.set_target(to_scale(u16::MAX, u16::MIN, upper_axis))?;
+        set_jrk(jrk, JrkMapping::UpperAxis, upper_axis)?;
     }
     if let Some(lower_axis) = pose.lower_axis {
-        jrk.set_device(13);
-        jrk.set_target(to_scale(u16::MAX, u16::MIN, lower_axis))?;
+        set_jrk(jrk, JrkMapping::LowerAxis, lower_axis)?;
     }
 
     if let Some(pitch_axis) = pose.pitch_axis {
-        jrk.set_device(14);
-        jrk.set_target(to_scale(u16::MAX, u16::MIN, pitch_axis))?;
+        set_jrk(jrk, JrkMapping::Pitch, pitch_axis)?;
     }
+    Ok(())
+}
+
+/// sets an individual JRK based on its I2C address.
+fn set_jrk(jrk: &mut JrkI2c2, target: JrkMapping, value: f32) -> Result<(), stm32f4xx_hal::i2c::Error> {
+    jrk.set_device(target.into());
+    jrk.set_target(to_scale(u16::MAX, u16::MIN, value))?;
+
     Ok(())
 }
